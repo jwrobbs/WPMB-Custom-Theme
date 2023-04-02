18f2868cc698df404d8d93fd14974c3ac191b8e9
Josh Robbs <josh@joshrobbs.com>
2023-04-02 09:30:52 -0400

    Add barebones header

--- header.php
+++ header.php
@@ -1,7 +1,23 @@
 <?php
 /**
- * Header template
+ * Header theme template
  *
  * @package wpmb
  * @added 02APR2023
  */
+
+defined( 'ABSPATH' ) || exit;
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> >
+<?php
+do_action( 'wp_body_open' );

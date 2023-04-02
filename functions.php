bc45cf95d574202f0ac1e7f5f8e00237c9091169
Josh Robbs <josh@joshrobbs.com>
2023-04-02 10:54:35 -0400

    Remove Gutenberg cruft

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The required functions theme file
+ *
+ * Don't expect much in here.
+ *
+ * @package wpmb
+ * @added 2023-04-02
+ */
+
+// [] Make this a plugin.
+/**
+ * Remove Gutenberg/FSE cruft
+ *
+ * Based on:
+ *
+ * @link https://fullsiteediting.com/lessons/how-to-remove-default-block-styles/#h-how-to-remove-the-inline-styles-on-the-front
+ * @author Josh Robbs <josh@joshrobbs.com>
+ * @created 2023-04-02
+ *
+ * @return void
+ */
+function wpmb_remove_core_block_styles() {
+
+	wp_dequeue_style( 'wp-block-columns' );
+	wp_dequeue_style( 'wp-block-column' );
+	wp_dequeue_style( 'global-styles' );
+
+	global $wp_styles;
+
+	foreach ( $wp_styles->queue as $key => $handle ) {
+		if ( strpos( $handle, 'wp-block-' ) === 0 ) {
+			wp_dequeue_style( $handle );
+		}
+	}
+}
+add_action( 'wp_enqueue_scripts', 'wpmb_remove_core_block_styles' );
+
--- funtions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * The required functions theme file
- *
- * Don't expect much in here.
- *
- * @package wpmb
- * @added 02APR2023
- */

bc417f187d64a5e05d95ea0f8dd40e537d4efda1
Josh Robbs <josh@joshrobbs.com>
2023-04-06 16:02:42 -0400

    Basic page done

--- singular.php
+++ singular.php
@@ -16,9 +16,11 @@ get_header();
 	// 1. get page id.
 	// 2. get page content.
 	// 3. display page content.
-	$page_id  = get_the_ID();
-	$page_obj = get_post( $page_id );
-	$content  = apply_filters( 'the_content', $page_obj->post_content );
+	$page_id    = get_the_ID();
+	$page_obj   = get_post( $page_id );
+	$content    = apply_filters( 'the_content', $page_obj->post_content );
+	$page_title = apply_filters( 'the_title', $page_obj->post_title );
+	echo wp_kses_post( "<h1>$page_title</h1>" );
 	echo wp_kses_post( $content );
 
 	?>

c095c5f0765dcbdf26c7178be3cdc97718435c6e
Josh Robbs <josh@joshrobbs.com>
2023-04-06 16:04:04 -0400

    Create page template from singular

---
+++ page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Page template
+ *
+ * @package wpmb
+ * @added 2023-04-06
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+<div class='container'>
+	<div class='container-inner'>
+	<?php
+	// 1. get page id.
+	// 2. get page content.
+	// 3. display page content.
+	$page_id    = get_the_ID();
+	$page_obj   = get_post( $page_id );
+	$content    = apply_filters( 'the_content', $page_obj->post_content );
+	$page_title = apply_filters( 'the_title', $page_obj->post_title );
+	echo wp_kses_post( "<h1>$page_title</h1>" );
+	echo wp_kses_post( $content );
+
+	?>
+</div></div>
+<?php
+get_footer();

5ea87bfcb3f56db960fdfceb87c84c8a857538a2
Josh Robbs <josh@joshrobbs.com>
2023-04-05 20:31:51 -0400

    Add footer styles and functions

--- footer.php
+++ footer.php
@@ -10,9 +10,24 @@ defined( 'ABSPATH' ) || exit;
 
 ?>
 </div> <!-- end of #content -->
-<footer>
-	<h2>I'm the footer.</h2>
-</footer>
+<div class='container footer-wrapper'>
+	<footer>
+		<div class='footer-section footer-biz-data'>
+			<!-- this should be a function or template -->
+			<h2 class=footer-title>WP MASTER BUILDER</h2>
+			<div>©2023 Josh Robbs</div>
+		</div>
+		<div class='footer-section footer-pages'>
+			<?php echo wp_kses_post( wpmb_get_footer_pages() ); ?>
+		</div>
+		<div class='footer-section footer-categories'>
+		<?php echo wp_kses_post( wpmb_get_footer_categories() ); ?>
+		</div>
+		<div class='footer-section footer-posts'>
+		<?php echo wp_kses_post( wpmb_get_footer_posts() ); ?>
+		</div>
+	</footer>
+</div>
 <?php wp_footer(); ?>
 
 </body>
--- functions.php
+++ functions.php
@@ -45,7 +45,6 @@ add_action( 'wp_enqueue_scripts', 'wpmb_remove_core_block_styles' );
  * @return void
  */
 function wpmb_theme_setup() {
-	wp_enqueue_style( 'style', get_stylesheet_uri(), false, filemtime( 'style.css' ) );
 
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'post-thumbnails' );
@@ -58,3 +57,98 @@ function wpmb_theme_setup() {
 
 }
 add_action( 'after_setup_theme', 'wpmb_theme_setup' );
+
+/**
+ * Enqueue theme style
+ *
+ * @author Josh Robbs <josh@joshrobbs.com>
+ * @created 2023-04-02
+ *
+ * @return void
+ */
+function wpmb_enqueue_theme_style() {
+	wp_enqueue_style(
+		'style',
+		get_stylesheet_uri(),
+		false,
+		filemtime( get_stylesheet_directory() . '/style.css' )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'wpmb_enqueue_theme_style' );
+
+// [] convert to more reusable plugin.
+/**
+ * Show pages in footer
+ *
+ * @author Josh Robbs <josh@joshrobbs.com>
+ * @created 2023-04-02
+ *
+ * @return void
+ */
+function wpmb_get_footer_pages() {
+	echo '<h3>Pages</h3>';
+	wp_nav_menu(
+		array(
+			'theme_location' => 'footer',
+			'items_wrap'     => '<ul role="list" id="menu-footer-menu" class="menu">%3$s</ul>',
+		)
+	);
+}
+/**
+ * Show cats in footer
+ *
+ * @author Josh Robbs <josh@joshrobbs.com>
+ * @created 2023-04-02
+ *
+ * @return string
+ */
+function wpmb_get_footer_categories() {
+	echo '<h3>Categories</h3>';
+
+	$categories = get_categories( // Returns array of objects.
+		array(
+			'orderby'    => 'name',
+			'order'      => 'ASC',
+			'hide_empty' => 1,
+		)
+	);
+	// Echo an unordered list of $categories with links.
+	ob_start();
+	if ( 0 < count( $categories ) ) {
+		echo '<ul role="list">';
+		foreach ( $categories as $category ) {
+			echo '<li><a href="' . esc_url( get_category_link( $category->term_id ) ) . '">' . esc_html( $category->name ) . '</a></li>';
+		}
+		echo '</ul>';
+	}
+	return ob_get_clean();
+}
+
+/**
+ * Show recent posts in footer
+ *
+ * @author Josh Robbs <josh@joshrobbs.com>
+ * @created 2023-04-02
+ *
+ * @return string
+ */
+function wpmb_get_footer_posts() {
+	echo '<h3>Recent posts</h3>';
+	$recent_posts = wp_get_recent_posts(
+		array(
+			'numberposts' => 5,
+			'post_status' => 'publish',
+		)
+	);
+
+	ob_start();
+	if ( 0 < count( $recent_posts ) ) {
+		echo '<ul role="list">';
+		foreach ( $recent_posts as $recent ) {
+			echo '<li><a href="' . esc_url( get_permalink( $recent['ID'] ) ) . '">' . esc_html( $recent['post_title'] ) . '</a></li>';
+		}
+		echo '</ul>';
+	}
+	return ob_get_clean();
+}
+

f746a107a65fe2a561900b6f4519b474b5772e0f
Josh Robbs <josh@joshrobbs.com>
2023-04-06 15:59:39 -0400

    Basic content added

--- home.php
+++ home.php
@@ -9,7 +9,17 @@
 get_header();
 ?>
 <div class='container'>
-	<h1>test 1</h1>
-</div>
+	<div class='container-inner'>
+	<?php
+	// 1. get page id.
+	// 2. get page content.
+	// 3. display page content.
+	$page_id  = get_the_ID();
+	$page_obj = get_post( $page_id );
+	$content  = apply_filters( 'the_content', $page_obj->post_content );
+	echo wp_kses_post( $content );
+
+	?>
+</div></div>
 <?php
 get_footer();
--- singular.php
+++ singular.php
@@ -10,8 +10,18 @@ defined( 'ABSPATH' ) || exit;
 
 get_header();
 ?>
-<div class='wrapper'>
-	<h1>test 1</h1>
-</div>
+<div class='container'>
+	<div class='container-inner'>
+	<?php
+	// 1. get page id.
+	// 2. get page content.
+	// 3. display page content.
+	$page_id  = get_the_ID();
+	$page_obj = get_post( $page_id );
+	$content  = apply_filters( 'the_content', $page_obj->post_content );
+	echo wp_kses_post( $content );
+
+	?>
+</div></div>
 <?php
 get_footer();

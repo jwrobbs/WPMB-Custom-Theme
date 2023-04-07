f9a894c29ca38d6a9195d0802e99f0435a38ab48
Josh Robbs <josh@joshrobbs.com>
2023-04-06 20:51:15 -0400

    Small refactor

--- template-parts/general-page.php
+++ template-parts/general-page.php
@@ -6,12 +6,8 @@
  * @added 2023-04-06
  */
 
-// 1. get page id.
-// 2. get page content.
-// 3. display page content.
-$page_id    = get_the_ID();
-$page_obj   = get_post( $page_id ); // ?? Is this necessary? Can't we just use $post?
-$content    = apply_filters( 'the_content', $page_obj->post_content );
-$page_title = apply_filters( 'the_title', $page_obj->post_title );
+// Get the post title and content from $post.
+$page_title = apply_filters( 'the_title', $post->post_title );
+$content    = apply_filters( 'the_content', $post->post_content );
 echo wp_kses_post( "<h1>$page_title</h1>" );
 echo wp_kses_post( $content );

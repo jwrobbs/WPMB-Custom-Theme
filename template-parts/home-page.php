5dc52dc1904d76110b95d6f107b6f6241ef1c9fd
Josh Robbs <josh@joshrobbs.com>
2023-04-16 09:19:58 -0400

    Make part completely php driven

--- template-parts/home-page.php
+++ template-parts/home-page.php
@@ -6,3 +6,13 @@
  * @added 2023-04-06
  */
 
+?>
+<div class='home-hero'>
+Hero
+</div>
+<div class='recent-activity'>
+recent
+</div>
+<div class='site-info'>site</div>
+<div class='home-cta'>cta
+</div>

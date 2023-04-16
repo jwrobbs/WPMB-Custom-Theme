8054138b3e37d0269efc6e8fcf6f3c27be15f8a8
Josh Robbs <josh@joshrobbs.com>
2023-04-16 09:18:26 -0400

    Make page completely php based

--- home.php
+++ home.php
@@ -6,20 +6,16 @@
  * @added 02APR2023
  */
 
-get_header();
+get_header( 'null' );
 ?>
-<div class='container'>
-	<div class='container-inner'>
-	<?php
-	// 1. get page id.
-	// 2. get page content.
-	// 3. display page content.
-	$page_id  = get_the_ID();
-	$page_obj = get_post( $page_id );
-	$content  = apply_filters( 'the_content', $page_obj->post_content );
-	echo wp_kses_post( $content );
-
-	?>
-</div></div>
+<div class='home-hero container'>
+	<h1>WP Master Builder</h1>
+	<h2>Project-centric WordPress training for developers</h2>
+	<div class="buttons">
+		<a href="/articles" class="btn">Articles</a>
+		<a href=/projects" class="btn">Projects</a>
+		<a href="/newsletter" class="btn">Newsletter</a>
+	</div>
+</div>
 <?php
-get_footer();
+get_footer( 'null' );

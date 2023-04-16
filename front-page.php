f9d2b69943b614965e6e27a250a0433e85c1fbad
Josh Robbs <josh@joshrobbs.com>
2023-04-16 09:20:25 -0400

    Add test content

---
+++ front-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Front page template
+ *
+ * @package wpmb
+ * @added 2023-04-15
+ */
+
+get_header( 'null' );
+?>
+<div class='home-hero container'>
+	<h1>WP Master Builder</h1>
+	<h2>Project-centric WordPress training for developers</h2>
+	<div class="buttons">
+		<a href="/articles" class="btn">Articles</a>
+		<a href=/projects" class="btn">Projects</a>
+		<a href="/newsletter" class="btn">Newsletter</a>
+	</div>
+</div>
+<?php
+get_footer( 'null' );
